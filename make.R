b656a07ac9cb5632364ebed7b0bab19172c9ef3c
‘Joel <joelnitta@gmail.com>
2020-03-16 07:45:28 +0000

    Make plan in non-global environment

--- make.R
+++ make.R
@@ -5,9 +5,7 @@
 # For more information about drake, see
 # https://ropensci.github.io/drake/
 
-# Setup ----
-
-# For reproducibility
+# Set random number seed for reproducibility
 set.seed(9130)
 
 # Set working directory
@@ -19,12 +17,17 @@ source("code/packages.R")
 # Update drake settings
 pkgconfig::set_config("drake::strings_in_dots" = "literals")
 
-# Load functions and plans  ----
+# Load functions and plans
 source("code/functions.R")
 source("code/plan.R")
 
 # Set cache
 nectandra_cache = new_cache("nectandra_cache")
 
-# Run analyses ----
-make(plan, cache = nectandra_cache) 
+# Specify non-global environment
+# to get around captioner modifying global env
+# (cf https://github.com/ropensci/drake/issues/749)
+envir <- new.env(parent = globalenv())
+
+# Run analyses
+make(plan, cache = nectandra_cache, envir = envir) 
