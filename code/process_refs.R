1588f0bba911badb1fa907793a031565e1bf9b82
‘Joel <joelnitta@gmail.com>
2020-03-23 07:18:53 +0000

    Use custom plos-one csl file
    Suppress page numbers in (whole) books

--- code/process_refs.R
+++ code/process_refs.R
@@ -7,4 +7,12 @@ make_ref_list(
   rmd_file = "ms/nectandra_pteridos.Rmd", 
   raw_bib = "ms/references_raw.bib",
   final_bib = "ms/references.bib",
-  strip_fields = "abstract")
+  strip_fields = "abstract",
+  exclude = c("Thiers2020", "oet2019"))
+
+# To edit the PLoS ONE style, go to
+# https://csl.mendeley.com/ and use the visual editor.
+# Save the CSL file as 'plos-jhn-2'
+# Download to the 'ms' folder:
+read_lines("https://csl.mendeley.com/styles/25428611/plos-jhn-2") %>%
+  write_lines(csl, "ms/plos-one.csl")
