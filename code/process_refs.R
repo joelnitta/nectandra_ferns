67977972d49a25700d939323740441dc96272272
‘Joel <joelnitta@gmail.com>
2020-03-23 07:20:33 +0000

    Fix group author names in .bib file

--- code/process_refs.R
+++ code/process_refs.R
@@ -10,6 +10,21 @@ make_ref_list(
   strip_fields = "abstract",
   exclude = c("Thiers2020", "oet2019"))
 
+# Make some manual fixes to authors in SI bibliography
+# (these are institutions, so need double brackets to
+# avoid latex thinking they have first and last names)
+read_lines("ms/references.bib") %>%
+  str_replace(
+    "Pteridophyte Phylogeny Group I",
+    "\\{Pteridophyte Phylogeny Group I\\}") %>%
+  str_replace(
+    "R Core Team",
+    "\\{R Core Team\\}") %>%
+  str_replace(
+    "CBOL Plant Working Group",
+    "\\{CBOL Plant Working Group\\}") %>%
+  write_lines("ms/references.bib")
+
 # To edit the PLoS ONE style, go to
 # https://csl.mendeley.com/ and use the visual editor.
 # Save the CSL file as 'plos-jhn-2'
