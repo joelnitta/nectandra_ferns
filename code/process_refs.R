d71e7b27e81c7e3753cddb0e7eceb7368676c5a5
Joel Nitta <joelnitta@gmail.com>
2020-09-07 07:52:50 +0000

    Update bibliography cleaning script to use bib exported from zotero

--- code/process_refs.R
+++ code/process_refs.R
@@ -1,28 +1,45 @@
 ### Set up bibliography and citation style files ###
 
-library(jntools)
 library(tidyverse)
+library(assertr)
+library(RefManageR)
 
-# Clean bibliography exported from Mendely and write it out.
-clean_bib(
-  "ms/references_raw.bib", 
-  strip_fields = c("abstract", "file", "keywords", 
-                   "publisher", 
-                   "url", "issn",
-                   "isbn", "month", "number")) %>%
-  write_lines("ms/references.bib")
+# Filter bibtex bibliography ----
 
-# Make some manual fixes to authors
-# (these are institutions, so need double brackets to
-# avoid latex thinking they have first and last names)
-read_lines("ms/references.bib") %>%
-  str_replace(
-    "Pteridophyte Phylogeny Group I",
-    "\\{Pteridophyte Phylogeny Group I\\}") %>%
-  str_replace(
-    "CBOL Plant Working Group",
-    "\\{CBOL Plant Working Group\\}") %>%
-  write_lines("ms/references.bib")
+# Set path to RMD file
+rmd_file <- "ms/nectandra_ferns.Rmd"
+
+# Parse RMD file and extract citation keys
+citations <- 
+  readr::read_lines(rmd_file) %>% 
+  stringr::str_split(" |;") %>% 
+  unlist %>% 
+  magrittr::extract(., stringr::str_detect(., "@")) %>% 
+  stringr::str_remove_all("\\[|\\]|\\)|\\(|\\.$|,|\\{|\\}") %>% 
+  magrittr::extract(., stringr::str_detect(., "^@")) %>% 
+  stringr::str_remove_all("@") %>% 
+  unique %>% sort
+
+# Read in entire reference library exported from Zotero with BetterBibTex
+bib <- RefManageR::ReadBib("data_raw/main_library.bib", check = FALSE)
+
+# Convert reference library to dataframe
+bib_df <- as.data.frame(bib) %>% rownames_to_column("cite_key")
+
+# Make sure no citations are missing
+tibble(cite_key = citations) %>%
+  anti_join(bib_df, by = "cite_key") %>%
+  verify(nrow(.) == 0, success_fun = success_logical)
+
+# Filter bibliography to only cited references, write it out
+bib_filtered <- bib_df %>%
+  filter(cite_key %in% citations) %>%
+  # RefManageR::as.BibEntry() needs rownames to be citation keys
+  column_to_rownames("cite_key") %>%
+  RefManageR::as.BibEntry() %>%
+  RefManageR::WriteBib(file = "ms/references.bib")
+
+# Download edited PLoS ONE citation style ----
 
 # To edit the PLoS ONE style, go to
 # https://csl.mendeley.com/ and use the visual editor.
